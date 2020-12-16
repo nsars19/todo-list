193ce33ca9c1e8eb28c405c1f7cb74cb867ef6a1
Nick <nsardonini@gmail.com>
2020-12-16 13:26:47 -0500

    add todo factory logic
    sets values of new object equal to supplied object, otherwise sets values to empty strings. returns the object.

--- src/modules/todo.js
+++ src/modules/todo.js
@@ -1,3 +1,15 @@
-export const todo = () => {
+export const todo = (info = null) => {
+  const todoInfo = {
+    title: "",
+    description: "",
+    dueDate: "",
+    priority: "",
+    notes: [],
+  }
+  
+  for (let prop in info) {
+    todoInfo[prop] = info[prop]
+  }
 
+  return todoInfo
 }
