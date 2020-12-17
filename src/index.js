33d3b81ddef27e9e3c856a5c85101f3c18f21ae5
Nick <nsardonini@gmail.com>
2020-12-16 20:34:01 -0500

    add more test functions

--- src/index.js
+++ src/index.js
@@ -1,15 +1,8 @@
 import "./stylesheets/css/main.css"
 import { pubsub, project, todo, DOM } from './modules'
 
-// test implementation
-let eventHandler = pubsub()
-// let subscription = eventHandler.subscribe("logData", logData)
-// eventHandler.publish("logData", "the data")
-// subscription.unsubscribe()
 
-function logData(data) {
-  console.log(data) 
-}
+let eventHandler = pubsub()
 
 const dom = DOM()
 
@@ -24,11 +17,42 @@ dom.$('.nav').addEventListener('click', () => {
 
 function toggleNav() {
   let nav = dom.$('.nav')
-  nav.classList.length === 2 ? nav.classList.remove('nav-selected') 
-                             : nav.classList.add('nav-selected')
+  nav.classList.length === 2 ? dom.removeClass(nav, 'nav-selected') 
+                             : dom.addClass(nav, 'nav-selected')
 }
 function toggleSidebar() {
   let sidebar = dom.$('.sidebar')
   dom.toggleAttr(sidebar, "data-sidebar-active")
 }
 
+let testTodo = todo({
+  description: "a descriptor",
+  dueDate: "12/25/2020",
+  priority: "high",
+  notes: ["super important", "make sure it's done well!"]
+})
+let testTodo2 = todo({
+  description: "another todo",
+  dueDate: "12/12/2020",
+  priority: "low",
+  notes: ["get to it eventually", "don't spend too much time on this one"]
+})
+
+const createTodo = eventHandler.subscribe("createTodo", todo)
+let todos = [
+  eventHandler.publish("createTodo", testTodo),
+  eventHandler.publish("createTodo", testTodo2)
+]
+
+let testProject = {
+  title: "new project",
+  todos: todos,
+}
+const proj = project(testProject)
+console.log(proj)
+
+function toggleNotes() {
+  let notes = dom.$('.todo-notes')
+  dom.toggleAttr(notes, "data-note-active")
+}  
+document.getElementById("1").addEventListener('click', toggleNotes)
