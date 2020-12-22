124071505af79a3852aacab9876cab84c68dbc85
Nick <nsardonini@gmail.com>
2020-12-21 22:22:25 -0500

    add application logic

--- src/index.js
+++ src/index.js
@@ -1,77 +1,84 @@
 import "./stylesheets/css/main.css"
 import { pubsub, project, todo, DOM } from './modules'
 
+const app = (() => {
+  const eventHandler = pubsub()
+  const dom = DOM()
+  let projects = [], 
+      todos    = [];
 
-let eventHandler = pubsub()
+  // Set up pub/sub subscriptions
+  let data = [
+    ["startApp", dom.createTodoForm],
+    ["startApp", dom.createProjectForm],
+    ["startApp", _setProjectButtonListeners],
+    ["startApp", _createDefaultList],
+    ["newProject", dom.showProjectForm],
+    ["newProject", dom.toggleSidebarAndNav],
+  ]
 
-const dom = DOM()
-
-dom.$('.nav').addEventListener('click', () => {
-  let toggle = eventHandler.subscribe("toggleNav", toggleNav)
-  eventHandler.publish("toggleNav")
-  toggle.unsubscribe()
-  let sidebar = eventHandler.subscribe("toggleSidebar", toggleSidebar)
-  eventHandler.publish("toggleSidebar")
-  sidebar.unsubscribe()
-})
-
-function toggleNav() {
-  let nav = dom.$('.nav')
-  nav.classList.length === 2 ? dom.removeClass(nav, 'nav-selected') 
-                             : dom.addClass(nav, 'nav-selected')
-}
-function toggleSidebar() {
-  let sidebar = dom.$('.sidebar')
-  dom.toggleAttr(sidebar, "data-sidebar-active")
-}
-
-function toggleNotes() {
-  let notes = dom.$('.todo-desc')
-  dom.toggleAttr(notes, "data-note-active")
-}
-
-let testTodo = todo({
-  todoTitle: "make a title",
-  description: "a descriptor",
-  dueDate: "12/25/2020",
-  priority: "high",
-})
-let testTodo2 = todo({
-  description: "another todo",
-  dueDate: "12/12/2020",
-  priority: "low",
-  notes: ["get to it eventually", "don't spend too much time on this one"]
-})
-let testTodo3 = todo({
-  description: "get cheese ingredients",
-  dueDate: "12/11/2020",
-  priority: "low",
-})
-let testTodo4 = todo({
-  description: "eatCHEESE and then get app running",
-  dueDate: "12/12/2020",
-  priority: "low",
-})
-const createTodo = eventHandler.subscribe("createTodo", todo)
-
-let project1 = project({
-  projectTitle: "Make some cheese",
-  todos: [testTodo3, testTodo4]
-})
-let project2 = project({
-  projectTitle: "Something else!",
-  todos: [testTodo, testTodo2]
-})
-
-let projects = [
-  {obj: project1, elem: dom.printProjectToSidebar(project1)},
-  {obj: project2, elem: dom.printProjectToSidebar(project2)}
-]
-
-projects.forEach(proj => {
-  proj["elem"].addEventListener('click', () => {
-    dom.clearFocus()
-    dom.printProjectToFocus(proj["obj"])
-    toggleSidebar()
+  data.forEach(sub => {
+    eventHandler.subscribe(sub[0], sub[1])
   })
-})
+
+  function _setProjectButtonListeners() {
+    _projectSubmit()
+    _projectCancel()
+  }
+  function _projectSubmit() {
+    const btn = dom.$('.project-submit')
+    btn.addEventListener('click', () => {
+      let proj = _buildProject()
+      dom.changeViewOnSubmit(proj["project"])
+      attachClickListenerToProject(proj["project"], proj["elem"])
+    })
+  }
+  function _projectCancel() {
+    dom.$('.project-cancel').addEventListener('click', () => {
+      dom.clearProjectForm()
+      dom.hideProjectForm()
+    })
+  }
+  function _buildProject() {
+    let data = dom.returnInfoFromProjectForm()
+    let proj = project(data)
+    let elem = dom.printProjectToSidebar(proj)
+    return projects[projects.push({project: proj, elem: elem}) - 1]
+  }
+  function attachClickListenerToProject(project, elem) {
+    elem.addEventListener('click', () => {
+      dom.printProjectToFocus(project)
+      dom.toggleSidebarAndNav()
+    })
+  }
+  // Adds a 'starter' list with some todos
+  function _createDefaultList() {
+    const defaultTodo = todo({
+      todoTitle: "Make another todo",
+      description: "add a description to each todo.",
+      dueDate: "12/25/2020",
+      priority: "high",
+    })
+    const defaultTodo2 = todo({
+      todoTitle: "Delete the first todo",
+      description: "which would then make this the first todo...",
+      dueDate: "3/14/2021",
+      priority: "medium",
+    })
+    const defaultProject = project({
+      projectTitle: "Make a todo list",
+      todos: [defaultTodo, defaultTodo2],
+    })
+    const projObj = {
+      project: defaultProject,
+      elem: dom.printProjectToSidebar(defaultProject)
+    }  
+
+    dom.printProjectToFocus(projObj["project"])
+    attachClickListenerToProject(projObj.project, projObj.elem)
+  }
+
+  dom.$('.nav').addEventListener('click', dom.toggleSidebarAndNav)
+  dom.id('createProject').addEventListener('click', eventHandler.publish.bind(this, "newProject"))
+  eventHandler.publish("startApp")
+})()
