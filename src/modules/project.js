f9a0899efc1caa55d7578a846dbaccc25ea2d2b0
Nick <nsardonini@gmail.com>
2020-12-23 17:56:28 -0500

    add method to add todos to project objects todos property

--- src/modules/project.js
+++ src/modules/project.js
@@ -2,5 +2,6 @@ export const project = (info) => {
   return {
     projectTitle: info["projectTitle"] || "",
     todos: info["todos"] || [],
+    addTodo: function(todo) { this.todos.push(todo) }
   }
 }
