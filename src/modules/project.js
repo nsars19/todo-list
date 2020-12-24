527507037c1d62c77dd08181b04d726978d6352e
Nick <nsardonini@gmail.com>
2020-12-24 18:59:44 -0500

    add removeTodo method to project object

--- src/modules/project.js
+++ src/modules/project.js
@@ -2,6 +2,7 @@ export const project = (info) => {
   return {
     projectTitle: info["projectTitle"] || "Unnamed Project",
     todos:        info["todos"]        || [],
-    addTodo: function(todo) { this.todos.push(todo) }
+    addTodo: function(todo) { this.todos.push(todo) },
+    removeTodo: function(idx) { this.todos.splice(idx, 1) }
   }
 }
