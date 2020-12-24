091baf2128c3d9539bf2a48a6090acf34c7ceb41
Nick <nsardonini@gmail.com>
2020-12-24 13:19:52 -0500

    add default project title

--- src/modules/project.js
+++ src/modules/project.js
@@ -1,7 +1,7 @@
 export const project = (info) => {
   return {
-    projectTitle: info["projectTitle"] || "",
-    todos: info["todos"] || [],
+    projectTitle: info["projectTitle"] || "Unnamed Project",
+    todos:        info["todos"]        || [],
     addTodo: function(todo) { this.todos.push(todo) }
   }
 }
