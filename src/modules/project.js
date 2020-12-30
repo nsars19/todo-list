ddaad0778c543371a64f58f06c13bbd6d9c28134
Nick <nsardonini@gmail.com>
2020-12-30 16:09:50 -0500

    change anonymous project functions to methods on the returned project object

--- src/modules/project.js
+++ src/modules/project.js
@@ -2,7 +2,7 @@ export const project = (info) => {
   return {
     projectTitle: info["projectTitle"] || "Unnamed Project",
     todos:        info["todos"]        || [],
-    addTodo: function(todo) { this.todos.push(todo) },
-    removeTodo: function(idx) { this.todos.splice(idx, 1) }
+    addTodo(todo) { this.todos.push(todo) },
+    removeTodo(idx) { this.todos.splice(idx, 1) }
   }
 }
